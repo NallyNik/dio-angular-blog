5fa6246018095a2d84557e35ca28510c998ccb49
NallyNik <nallynik@gmail.com>
2023-04-24 16:59:58 -0300

    update components

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,27 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "photo": "https://source.unsplash.com/random/?cat?sig=\$1",
+    "title": "Gatos são os melhores",
+    "description": "Os gatos são os melhores animais de estimação por muitas razões. Em primeiro lugar, eles são independentes e autossuficientes, o que significa que não precisam de tanta atenção e cuidado quanto os cães, por exemplo. Isso os torna ideais para pessoas ocupadas que ainda desejam a companhia de um animal de estimação."
+  }
+,
+  {
+    "id": "2",
+    "photo": "https://source.unsplash.com/random/?cat?sig=\$2",
+    "title": "Cotidiano com gatos",
+    "description": "O cotidiano com gatos é uma experiência única e enriquecedora. Começa pela rotina de alimentação, que geralmente envolve dar comida e água fresca ao seu gato, bem como limpar sua caixa de areia. Em seguida, os momentos de brincadeira e interação, seja com brinquedos ou apenas com carinhos e afagos, ajudam a estreitar os laços entre você e seu gato."
+  },
+  {
+    "id": "3",
+    "photo": "https://source.unsplash.com/random/?cat?sig=\$3",
+    "title": "Buscando seu próximo amigo",
+    "description": "Antes de adotar um gato, é importante considerar alguns pré-requisitos. Primeiramente, é preciso ter tempo e disponibilidade para cuidar do animal, incluindo a alimentação, higiene e atenção necessárias. Além disso, é preciso ter um espaço adequado para o gato, com áreas para dormir, brincar e fazer suas necessidades. É importante lembrar também que a adoção de um gato implica em um compromisso de longo prazo, já que a expectativa de vida desses animais pode chegar a mais de 15 anos. Portanto, é necessário estar disposto a assumir esse compromisso e a oferecer um lar seguro e amoroso para o gato."
+  },
+  {
+    "id": "4",
+    "photo": "https://source.unsplash.com/random/?cat?sig=\$4",
+    "title": "Como passear com seu bichano",
+    "description": "Diferente dos cachorros, os gatos não precisam ser passeados diariamente, mas alguns gatos podem gostar de sair e explorar o mundo ao ar livre. Nesses casos, é importante ter em mente que os gatos são animais territoriais e podem se sentir inseguros em ambientes desconhecidos. Por isso, é recomendado utilizar uma coleira própria para gatos e começar com passeios curtos e em locais tranquilos, para que o gato possa se acostumar e sentir-se seguro. É importante estar sempre atento ao comportamento do gato durante o passeio, respeitando seu ritmo e evitando situações que possam assustá-lo."
+  }
+]
